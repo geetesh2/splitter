19d90014c4e20902c351902b2038de48ace4b0c5
Geetesh <geeteshpandey2@gmail.com>
2025-02-04 22:41:25 +0530

    still not working for components

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SplitterComponent } from "../splitter/splitter.component";
 import { MatCard,MatCardTitle,MatCardContent } from '@angular/material/card';
+import { ContentComponent } from "../content/content.component";
 
 @Component({
   selector: 'app-example',
   standalone: true,
-  imports: [SplitterComponent,MatCard,MatCardTitle,MatCardContent],
+  imports: [SplitterComponent, MatCard, MatCardTitle, MatCardContent, ContentComponent],
   templateUrl: './example.component.html',
   styleUrl: './example.component.css'
 })
